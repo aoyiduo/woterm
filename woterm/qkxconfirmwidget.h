0a6aaa3124037e4126b7b88dffd213035b46ce34
wingo <wingo@aoyiduo.com>
2025-09-17 09:37:16 +0800

    changed gpl license to apache-2.0

--- woterm/qkxconfirmwidget.h
+++ woterm/qkxconfirmwidget.h
@@ -4,8 +4,8 @@
 *
 * Contact: http://www.aoyiduo.com
 *
-*   this file is used under the terms of the GPLv3[GNU GENERAL PUBLIC LICENSE v3]
-* more information follow the website: https://www.gnu.org/licenses/gpl-3.0.en.html
+*   this file is used under the terms of the Apache License, Version 2.0
+* more information follow the website: https://www.apache.org/licenses/LICENSE-2.0.txt
 *
 *******************************************************************************************/
 #ifndef QKXCONFIRMWIDGET_H
